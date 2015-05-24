f6db0e4ebf58491927e97d79190a12d411d64187
Milano Gadelha Carvalho <milanogc@gmail.com>
2015-05-24 20:37:18 -0300

    Primeiro Servlet.

---
+++ src/main/java/com/milanogc/servlet/OiMundo.java
@@ -0,0 +1,20 @@
+package com.milanogc.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/oi-mundo")
+public class OiMundo extends HttpServlet {
+  protected void service (HttpServletRequest request,
+      HttpServletResponse response)
+      throws ServletException, IOException {
+    PrintWriter out = response.getWriter();
+    out.println("Oi, mundo!");
+  }
+}
